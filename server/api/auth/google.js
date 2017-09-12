be482d930348396d29795ed140feb7eb1392815c
Sarah Denaci <sarahdenaci@gmail.com>
2017-09-12 17:26:11 -0400

    feature: add Google OAuth strategy

--- server/api/auth/google.js
+++ server/api/auth/google.js
@@ -2,30 +2,44 @@ const router = require('express').Router();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// don't forget to install passport-google-oauth
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-passport.use(
-  new GoogleStrategy({
-    clientID: '939799157183-oj74cvfn68dkvb4gblsl2bqh8kddba4k.apps.googleusercontent.com',
-    clientSecret: 'YOUR_APP_SECRET',
-    callbackURL: 'YOUR_CALLBACK_URL'
-  },
-  // Google will send back the token and profile
-  function (token, refreshToken, profile, done) {
-    // the callback will pass back user profile information and each service (Facebook, Twitter, and Google) will pass it back a different way. Passport standardizes the information that comes back in its profile object.
-    /*
-    --- fill this part in ---
-    */
-  })
-);
+// // don't forget to install passport-google-oauth
+  const googleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/api/auth/google/verify'
+  };
 
-// Google authentication and login 
-app.get('/', passport.authenticate('google', { scope: 'email' }));
+const strategy = new GoogleStrategy(googleConfig, function (token, refreshToken, profile, done) {
+  const googleId = profile.id;
+  const name = profile.displayName;
+  const email = profile.emails[0].value;
+
+  User.findOne({where: { googleId: googleId  }})
+    .then(function (user) {
+      if (!user) {
+        return User.create({ name, email, googleId })
+          .then(function (user) {
+            done(null, user);
+          });
+      } else {
+        done(null, user);
+      }
+    })
+    .catch(done);
+});
+
+// register our strategy with passport
+passport.use(strategy);
+
+// Google authentication and login
+router.get('/', passport.authenticate('google', { scope: 'email' }));
 
 // handle the callback after Google has authenticated the user
-app.get('/callback',
+router.get('/verify',
   passport.authenticate('google', {
     successRedirect: '/', // or wherever
-    failureRedirect: '/' // or wherever
+    failureRedirect: '/login' // or wherever
   })
 );
+
+module.exports = router
