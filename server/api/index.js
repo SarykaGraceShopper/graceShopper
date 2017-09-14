bf9a530ec6fe429cf1f2865d474d30d0ac0e2026
Sarah Denaci <sarahdenaci@gmail.com>
2017-09-14 12:41:33 -0400

    fix: db bug

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,8 @@ router.use('/users', require('./users'));
 router.use('/reviews', require('./reviews'));
 router.use('/powers', require('./powers'));
 router.use('/auth', require('./auth'));
+router.use('/dragons', require('./dragons'));
+
 
 router.use(function (req, res, next) {
   const err = new Error('Not found.');
