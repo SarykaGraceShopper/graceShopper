a462e0302d064d290ea78b6f6c9022c2d41556b4
Mieka Page <git@coldwine.nyc>
2017-09-18 15:02:31 -0400

    finalize seed, add bootstrap buttons, add details to single dragon view

--- seed.js
+++ seed.js
@@ -3,41 +3,47 @@ const {db} = require('./server/db');
 
 const data = {
   dragon: [
-    {name: 'cookie', color: 'yellow', breed: 'qt', price: '200', badness: '1'},
+    {name: 'cookie', color: 'yellow', breed: 'sweettooth', price: '200', badness: '1'},
     {name: 'tiny', color: 'green', breed: 'qt', price: '200', badness: '1'},
-    {name: 'romeo', color: 'green', breed: 'qt', price: '200', badness: '1'},
-    {name: 'smore', color: 'orange', breed: 'qt', price: '200', badness: '1'},
-    {name: 'daria', color: 'aqua', breed: 'qt', price: '200', badness: '1'},
-    {name: 'fat cat', color: 'green', breed: 'qt', price: '200', badness: '1'},
+    {name: 'romeo', color: 'green', breed: 'dreamer', price: '200', badness: '1'},
+    {name: 'smore', color: 'orange', breed: 'camper', price: '200', badness: '1'},
+    {name: 'daria', color: 'grey', breed: 'plastic', price: '200', badness: '1'},
+    {name: 'fat cat', color: 'agua', breed: 'sweettooth', price: '200', badness: '1'},
     {name: 'ken', color: 'green', breed: 'qt', price: '200', badness: '1'},
-    {name: 'rawr', color: 'green', breed: 'qt', price: '200', badness: '1'},
-    {name: 'juliet', color: 'green', breed: 'qt', price: '200', badness: '1'},
+    {name: 'rawr', color: 'green', breed: 'dreamer', price: '200', badness: '1'},
+    {name: 'juliet', color: 'green', breed: 'dreamer', price: '200', badness: '1'},
     {name: 'fira', color: 'green', breed: 'snake', price: '200', badness: '1'},
     {name: 'viserion', color: 'blue', breed: 'dead', price: '2000', badness: '11'},
     {name: 'bundle deal', color: 'multi', breed: 'neopet', price: '200', badness: '0'}
   ],
   power: [
-    {name: 'cookie-eating', destructiveness: '1'},
-    {name: 'fire', destructiveness: '8'},
-    {name: 'love', destructiveness: '5'},
+    {name: 'crunching', destructiveness: '1'},
+    {name: 'breathing fire', destructiveness: '8'},
+    {name: 'crushing', destructiveness: '1'},
     {name: 'judgement', destructiveness: '2'},
     {name: 'staring', destructiveness: '1'},
     {name: 'growling', destructiveness: '0'},
-    {name: 'ice', destructiveness: '10'}
+    {name: 'breathing ice', destructiveness: '10'},
+    {name: 'hissing', destructiveness: '0'},
+    {name: 'not so much', destructiveness: '0'},
+    {name: 'munching', destructiveness: '0'}
   ],
   dragon_power: [
     {dragonId: '1', powerId: '1'},
+    {dragonId: '1', powerId: '10'},
     {dragonId: '2', powerId: '2'},
     {dragonId: '3', powerId: '3'},
     {dragonId: '4', powerId: '2'},
     {dragonId: '5', powerId: '4'},
     {dragonId: '6', powerId: '5'},
-    {dragonId: '6', powerId: '1'},
+    {dragonId: '6', powerId: '10'},
     {dragonId: '7', powerId: '2'},
     {dragonId: '8', powerId: '3'},
     {dragonId: '8', powerId: '6'},
     {dragonId: '9', powerId: '3'},
-    {dragonId: '11', powerId: '7'}
+    {dragonId: '10', powerId: '8'},
+    {dragonId: '11', powerId: '7'},
+    {dragonId: '12', powerId: '9'}
   ]
 };
 
