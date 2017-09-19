65c5bd1f1b0d5ccec1428b08f65e8ad006dcaa71
Mieka Page <git@coldwine.nyc>
2017-09-19 13:09:34 -0400

    sorting dragons almost finished

--- client/components/AllDragons.jsx
+++ client/components/AllDragons.jsx
@@ -5,74 +5,129 @@ import {withRouter} from 'react-router'
 import history from '../history'
 import store from '../store'
 
+class AllDragons extends React.Component {
 
-function AllDragons(props) {
-
-  let filteredDragons = props.dragons;
-
-  let dragonColors = new Set();
-  let dragonBreeds = new Set();
-
-  props.dragons && props.dragons.forEach(dragon => {
-    for (var key in dragon) {
-      if (key === 'color') dragonColors.add(dragon[key]);
-      if (key === 'breed') dragonBreeds.add(dragon[key]);
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredDragons: [],
+      selectedKey: '',
+      selectedValue: ''
     }
-  });
-
-  dragonColors = Array.from(dragonColors);
-  dragonBreeds = Array.from(dragonBreeds);
-
-  function filterDragons(key, value) {
-    filteredDragons = props.dragons.filter(dragon => dragon[key] === value);
-    //FIGURE OUT HOW TO LOAD THESE
+    this.makeSets = this.makeSets.bind(this);
+    this.filterDragons = this.filterDragons.bind(this);
+    this.displayDragons = this.displayDragons.bind(this);
   }
 
-  return (
-    <div className="adjacent-jsx-elements-must-be-wrapped-in-an-enclosing-tag">
-      <div className="row">
-        <div className="col-sm-12 col-md-12 col-lg-12">
-          <h1>Dragons</h1>
-            narrow your search by&nbsp;
-            <select onChange={(event) => filterDragons('color', event.target.value)}>
-              <option>color</option>
-              {
-                dragonColors.map(color => (
-                  <option key={color} name={color} value={color}>{color}</option>
-                ))
-              }
-            </select> or&nbsp;
-            <select onChange={(event) => filterDragons('breed', event.target.value)}>
-              <option>breed</option>
-              {
-                dragonBreeds.map(breed => (
-                  <option key={breed} name={breed} value={breed}>{breed}</option>
-                ))
-              }
-            </select> or&nbsp;
-            <select onChange={(event) => filterDragons('badness', event.target.value)}>
-              <option>badness</option>
-              <option name="0-5" value="0-5">0-5</option>
-              <option name="6-10" value="6-10">6-10</option>
-              <option name="11" value="11">11</option>
-            </select>
+  makeSets(whichKey) {
+    let dragonSets = new Set();
+    this.props.dragons.length && this.props.dragons.forEach(dragon => {
+      for (var key in dragon) {
+        if (key === whichKey) dragonSets.add(dragon[key]);
+      }
+    });
+    return Array.from(dragonSets).sort();
+  }
+
+  filterDragons(key, value) {
+    this.setState({
+      filteredDragons: this.props.dragons.filter(dragon => dragon[key] === value),
+      selectedKey: key,
+      selectedValue: value.toString()
+    });
+  }
+
+  displayDragons() {
+    return (this.state.filteredDragons.length)
+      ? this.state.filteredDragons
+      : this.props.dragons;
+  }
+
+  render() {
+    return (
+      <div className="adjacent-jsx-elements-must-be-wrapped-in-an-enclosing-tag">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-12">
+            <h1>Dragons</h1>
+              narrow your search by&nbsp;
+              <select
+                onChange=
+                  {
+                    (event) => this.filterDragons('color', event.target.value)
+                  }
+                value=
+                  {
+                    (this.state.selectedKey === 'color')
+                      ? this.state.selectedValue
+                      : 'color'
+                  }
+                >
+                <option value="color">color</option>
+                {
+                  this.makeSets('color').map((color, index) => (
+                    <option key={index} name={color} value={color}>{color}</option>
+                  ))
+                }
+              </select> or&nbsp;
+              <select
+                onChange=
+                  {
+                    (event) => this.filterDragons('breed', event.target.value)
+                  }
+                value=
+                  {
+                    (this.state.selectedKey === 'breed')
+                      ? this.state.selectedValue
+                      : 'breed'
+                  }
+                >
+                <option value="breed">breed</option>
+                {
+                  this.makeSets('breed').map((breed, index) => (
+                    <option key={index} name={breed} value={breed}>{breed}</option>
+                  ))
+                }
+              </select> or&nbsp;
+              <select
+                onChange=
+                  {
+                    (event) => this.filterDragons('badness', event.target.value)
+                  }
+                value=
+                  {
+                    (this.state.selectedKey === 'badness')
+                      ? this.state.selectedValue
+                      : 'badness'
+                  }
+                >
+                <option value="badness">badness</option>
+                {
+                  this.makeSets('badness').map((badness, index) => (
+                    <option key={index} name={badness} value={badness}>{badness}</option>
+                  ))
+                }
+              </select>
+          </div>
+        </div>
+        <div className="row">
+          {
+            this.props.dragons && this.displayDragons().map(dragon => (
+            <div className="col-sm-6 col-md-6 col-lg-3" key={dragon.id}>
+              <Link to={`/dragons/${dragon.id}`}>
+                <h3>{dragon.name}</h3>
+                  <div className="dragon-image" style={{backgroundImage: `url(${dragon.image})`}}>
+                  </div>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
-      <div className="row">
-        {filteredDragons.length && filteredDragons.map(dragon => (
-          <div className="col-sm-6 col-md-6 col-lg-3" key={dragon.id}>
-            <Link to={`/dragons/${dragon.id}`}>
-              <h3>{dragon.name}</h3>
-                <div className="dragon-image" style={{backgroundImage: `url(${dragon.image})`}}>
-                </div>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
-const mapStateToProps = (state) => ({dragons: state.dragons});
+const mapStateToProps = (state) => ({
+  dragons: state.dragons
+});
 
 export default connect(mapStateToProps)(AllDragons);
